97d7b459fea82670b60b63ee12c099b479bcd540
mariowilmer <93395620+mariowilmer@users.noreply.github.com>
2021-12-14 16:45:03 -0500

    index mario
    14/12/21

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<!-- Bootstrap CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+<!-- Carrusel -->
+<link href="css/carousel.css" rel="stylesheet">
+ 
+<!-- Estilos Personalizados -->
+<link href="css/estilos.css" rel="stylesheet">
